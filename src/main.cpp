4e1f7514e3bd959a09a947017b57f3ca013360eb
Riley Weber <rileyw13@protonmail.com>
2019-12-28 20:05:54 -0500

    now prints every row of the table

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,53 @@
-#include <iostream>
+/* 
+ * with lots of help from
+ * https://www.tutorialspoint.com/sqlite/sqlite_c_cpp.htm
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
 #include <sqlite3.h> 
 
-using namespace std;
+static int callback(void *data, int argc, char **argv, char **azColName){
+   int i;
+   fprintf(stderr, "%s: ", (const char*)data);
+   
+   for(i = 0; i<argc; i++){
+      printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+   }
+   
+   printf("\n");
+   return 0;
+}
 
 int main(int argc, char* argv[]) {
    sqlite3 *db;
    char *zErrMsg = 0;
    int rc;
+   char *sql;
+   const char* data = "Callback function called";
 
+   /* Open database */
    rc = sqlite3_open("google-ctf-2019-hardware-minetest-map/map.sqlite", &db);
-
+   
    if( rc ) {
-      cerr << "Can't open database: " << sqlite3_errmsg(db) << endl;
+      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
       return(0);
    } else {
-      cerr << "opened database successfully!" << endl;
+      fprintf(stderr, "Opened database successfully\n");
+   }
+
+   /* Create SQL statement */
+   sql = "SELECT * from blocks";
+
+   /* Execute SQL statement */
+   rc = sqlite3_exec(db, sql, callback, (void*)data, &zErrMsg);
+   
+   if( rc != SQLITE_OK ) {
+      fprintf(stderr, "SQL error: %s\n", zErrMsg);
+      sqlite3_free(zErrMsg);
+   } else {
+      fprintf(stdout, "Operation done successfully\n");
    }
    sqlite3_close(db);
+   return 0;
 }
