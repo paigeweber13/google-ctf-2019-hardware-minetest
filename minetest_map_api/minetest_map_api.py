fe6f8f476e058cf593fa91cb4c95eed7cde42f9c
Riley <rileyw13@protonmail.com>
2019-12-31 13:56:05 -0500

    added check for if map exists

--- minetest_map_api/minetest_map_api.py
+++ minetest_map_api/minetest_map_api.py
@@ -1,6 +1,8 @@
+import os
 import sqlite3
 
 DB_NAME = 'google-ctf-2019-hardware-minetest-map/map.sqlite'
+MAP_DOWNLOAD_URL = 'https://capturetheflag.withgoogle.com/#challenges/hardware-minetest'
 
 def getBlockAsInteger(p):
     return int64(p[2]*16777216 + p[1]*4096 + p[0])
@@ -27,14 +29,22 @@ def unsignedToSigned(i, max_positive):
         return i - 2*max_positive
 
 def load_blocks():
-    conn = sqlite3.connect(DB_NAME)
-    c = conn.cursor()
-    c.execute('SELECT * FROM blocks')
-    # print(c.fetchone()[1].decode('ascii'))
-    return c.fetchall()
+    if os.path.isfile(DB_NAME):
+        conn = sqlite3.connect(DB_NAME)
+        c = conn.cursor()
+        c.execute('SELECT * FROM blocks')
+        # print(c.fetchone()[1].decode('ascii'))
+        return c.fetchall()
+    else:
+        print('couldn\'t find map file at ' + DB_NAME + '.')
+        print('you can download the map at ' + MAP_DOWNLOAD_URL)
+        return None
 
 def main():
     blocks = load_blocks()
+    if blocks == None:
+        return
+    
     first_block = blocks[0]
     print('pos:', getIntegerAsBlock(first_block[0]))
     print('block data:', first_block[1])
